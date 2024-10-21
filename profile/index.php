4d734d935c2626c06774f7d332ad29ecf5068a9b
Vaughn Gugger <vjgtigers@gmail.com>
2024-10-21 18:58:06 -0400

    work

--- profile/index.php
+++ profile/index.php
@@ -5,20 +5,6 @@ $username = htmlspecialchars($_GET["u"]);
 
 echo "username: " . $username . "<br>";
 
-$url = "http://127.0.0.1:5000/schedule/full/" . $username;
- 
-// Initialize a CURL session.
-$ch = curl_init(); 
- 
-// Return Page contents.
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
- 
-//grab URL and pass it to the variable.
-curl_setopt($ch, CURLOPT_URL, $url);
-
-$result = curl_exec($ch);
-echo "<br> Schedule: " . $result . "<br>";
-
 
 
 $url = "http://127.0.0.1:5000/userdata/" . $username;
@@ -35,7 +21,34 @@ echo "Major: " . $userdata->major . "<br>";
 echo "Minor: " . $userdata->minor . "<br>";
 echo "Year: " . $userdata->year . "<br>";
 
+$url = "http://127.0.0.1:5000/schedule/full/" . $username;
+$ch = curl_init(); 
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_URL, $url);
+$result = curl_exec($ch);
+echo "<br> Schedule: " . "<br><br>";
 
+$ex = explode("+", $result);
+
+foreach( $ex as $e) {
+	//echo $e . "<br>";
+	$a = explode("?", $e);
+	if($a[1] != "N") {
+		echo $a[0] . "<br>";
+		$s = explode(".", $a[1]);
+		foreach($s as $p) {
+			$r = explode("@",$p);
+			foreach($r as $o) {
+				echo '&nbsp'.'&nbsp' . $o;}
+		echo "<br>";
+}
+
+
+	}
+
+
+
+}
 
 
 ?>
