3867b56bdf63c0aa7ffa9fe9e2fa8ab48b5138e8
Vaughn Gugger <vjgtigers@gmail.com>
2024-10-21 15:35:15 -0400

    work

--- profile/index.php
+++ profile/index.php
@@ -17,7 +17,23 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_URL, $url);
 
 $result = curl_exec($ch);
-echo "<br> Schedule: " . $result;
+echo "<br> Schedule: " . $result . "<br>";
+
+
+
+$url = "http://127.0.0.1:5000/userdata/" . $username;
+$ch = curl_init(); 
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_URL, $url);
+
+$result = curl_exec($ch);
+//echo $result;
+
+$userdata = json_decode($result);
+echo "Name: " . $userdata->name . "<br>";
+echo "Major: " . $userdata->major . "<br>";
+echo "Minor: " . $userdata->minor . "<br>";
+echo "Year: " . $userdata->year . "<br>";
 
 
 
